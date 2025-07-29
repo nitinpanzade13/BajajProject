163973a85ed5a9a5b67863d91ce571d3f133c8eb
Vedant121613 <vedantwahile7@gmail.com>
2025-07-29 23:25:26 +0530

    Delete Bajaj-Finserve-PolicyPilot directory

--- Bajaj-Finserve-PolicyPilot/README.md
+++
@@ -1,2 +0,0 @@
-# Bajaj-Finserve-PolicyPilot
-A smart chatbot that navigates complex policy documents using RAG and LLMs, delivering precise coverage decisions and clause-backed explanations.
--- Bajaj-Finserve-PolicyPilot/utils.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-def download_pdf(url, save_path="temp.pdf"):
-    response = requests.get(url)
-    if response.status_code != 200:
-        raise Exception(f"Failed to download PDF: {response.status_code}")
-    with open(save_path, "wb") as f:
-        f.write(response.content)
-    return save_path
